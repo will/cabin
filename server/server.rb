22fa9e946ae61e639be12b5eb724deb875bd5bcd
Will Leinweber <will@bitfission.com>
2012-12-12 01:59:47 -0800

    show logs via websockets in browser

--- server/server.rb
+++ server/server.rb
@@ -1,17 +1,36 @@
 require 'sinatra'
+require 'sinatra-websocket'
 require 'heroku'
 
 module Cabin
   class Server < Sinatra::Application
+    def app
+      ENV['APP'] || 'will'
+    end
+
     get '/' do
+      @app = app
       erb :index
     end
 
     get '/logs' do
-      app = 'will'
-      stream(:keep_open) do |out|
-        EM::PeriodicTimer.new(20) { out << "\0" }
-        Heroku::Auth.client.read_logs(app, ['tail=1']) { |line| out << line }
+      if request.websocket?
+        request.websocket do |ws|
+          ws.onopen {
+            EM.next_tick {
+              # THREADS thank you
+              # https://github.com/sseforce/heroku_pusher/blob/master/app.rb
+              @thread = Thread.new do
+                Heroku::Auth.client.read_logs(app, ['tail=1']) { |line| ws.send line }
+              end
+            }
+          }
+       end
+      else # not websocket
+        stream(:keep_open) do |out|
+          EM::PeriodicTimer.new(20) { out << "\0" }
+          Heroku::Auth.client.read_logs(app, ['tail=1']) { |line| out << line }
+        end
       end
     end
 
